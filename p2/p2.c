440611e2a3bc5947ccb1af640bbe920b5364fe0a
swathigowda93 <swathigowda95430@gmail.com>
2024-11-08 13:08:57 +0530

    p2

---
+++ p2/p2.c
@@ -0,0 +1,12 @@
+nclude<stdio.h>
+int main()
+{
+    short int inputNumber = 0;
+    puts("Enter a number to check if it is Even");
+    scanf("%d", &inputNumber);
+    if (inputNumber % 2 == 0)
+        printf("%d is an Even number", inputNumber);
+    else
+        printf("%d is an Odd number", inputNumber);
+    return 0;
+}
